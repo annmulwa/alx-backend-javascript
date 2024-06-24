6d62e20dc080a76c426e073d016be29daf4ca650
annmulwa <annmulwa2000@gmail.com>
2024-06-24 13:43:55 +0000

    js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const NewArray = [];
+    for (const value of array) {
+      NewArray.push(appendString + value);
   }
 
-  return array;
+  return NewArray;
 }
