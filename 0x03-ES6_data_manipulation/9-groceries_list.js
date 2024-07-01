6bc28570157a7c4a8303997d3c70fae9199d0ba5
annmulwa <annmulwa2000@gmail.com>
2024-07-01 17:39:05 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-  const groceries = [
+  const values = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-  return new Map(groceries);
+  return new Map(values);
 }
