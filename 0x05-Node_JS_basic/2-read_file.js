87e286378ad808799f022c97fd302d9e0e71e970
annmulwa <annmulwa2000@gmail.com>
2024-08-19 18:35:05 +0300

    Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,28 +2,28 @@ const fs = require('fs');
 
 module.exports = function countStudents(path) {
   try {
-    // read data
-    const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    // split data and taking only list without header
-    const lines = data.split('\n').slice(1, -1);
-    // give the header of data
-    const header = data.split('\n').slice(0, 1)[0].split(',');
-    // find firstname and field index
-    const idxFn = header.findIndex((ele) => ele === 'firstname');
-    const idxFd = header.findIndex((ele) => ele === 'field');
+    // read file data Synchronously
+    const filedata = fs.readFileSync(path, { encoding: 'utf-8' });
+    // split data and taking the list without the header
+    const filelines = filedata.split('\n').slice(1, -1);
+    // header of file data
+    const header = filedata.split('\n').slice(0, 1)[0].split(',');
+    // find firstname and field index in the file
+    const indexfn = header.findIndex((elem) => elem === 'firstname');
+    const indexfd = header.findIndex((elem) => elem === 'field');
     // declarate two dictionaries for count each fields and store list of students
     const fields = {};
     const students = {};
 
-    lines.forEach((line) => {
+    filelines.forEach((line) => {
       const list = line.split(',');
-      if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
-      fields[list[idxFd]] += 1;
-      if (!students[list[idxFd]]) students[list[idxFd]] = '';
-      students[list[idxFd]] += students[list[idxFd]] ? `, ${list[idxFn]}` : list[idxFn];
+      if (!fields[list[indexfd]]) fields[list[indexfd]] = 0;
+      fields[list[indexfd]] += 1;
+      if (!students[list[indexfd]]) students[list[indexfd]] = '';
+      students[list[indexfd]] += students[list[indexfd]] ? `, ${list[indexfn]}` : list[indexfn];
     });
 
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${filelines.length}`);
     for (const key in fields) {
       if (Object.hasOwnProperty.call(fields, key)) {
         const element = fields[key];
@@ -34,4 +34,3 @@ module.exports = function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 };
-
